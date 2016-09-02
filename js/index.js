b4d4ede82d1c751dd6f3dfccb82d3186b8422928
dylanebert <dylan.ebert@gmail.com>
2016-09-01 23:13:37 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var line = document.getElementById("line");
+var length = line.getTotalLength();
+
+line.style.strokeDasharray = length;
+line.style.strokeDashoffset = length;
+
+window.addEventListener("scroll", myFunction);
+myFunction();
+
+function myFunction() {
+	var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+	var draw = length * scrollpercent * 0.5;
+	line.style.strokeDashoffset = length - draw;
+}
